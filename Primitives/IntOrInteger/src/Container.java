639ee2bbebfd534b29e34d5e6545cb5b586af268
Konstantin <nki-12@bk.ru>
2023-08-07 15:15:04 +0300

    HomeWork

--- Primitives/IntOrInteger/src/Container.java
+++ Primitives/IntOrInteger/src/Container.java
@@ -1,5 +1,8 @@
 public class Container {
-    private Integer count;
+    private Integer count = 0;
+    // Integer является объеком, следовательно по дефолту инициализируется как null, а значит ему просто нужно задать значение
+
+
 
     public void addCount(int value) {
         count = count + value;
