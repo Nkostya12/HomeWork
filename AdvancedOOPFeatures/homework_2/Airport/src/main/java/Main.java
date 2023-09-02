159e2f2e6511ffc4ab4990d72c55f0277074142d
Konstantin <nki-12@bk.ru>
2023-09-02 16:00:23 +0300

    hw

--- AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
@@ -1,7 +1,12 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
-import java.util.Collections;
-import java.util.List;
+import com.skillbox.airport.Terminal;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.*;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,8 +15,30 @@ public class Main {
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
+        Date dateNow = new Date();
+        long hours = 7200000L;
+        ArrayList<Flight> endTwoHours = new ArrayList<>();
+
+        ArrayList<Terminal> terminal = new ArrayList<>();
+
+        Stream<Terminal> stream = airport.getTerminals().stream();
+
+        for (Terminal terminal1 : airport.getTerminals()) {
+            System.out.println(terminal1.getFlights());
+            for (Flight flight : terminal1.getFlights()) {
+                long plus = flight.getDate().getTime() ;
+                if (minus >= hours ){
+                    endTwoHours.add(flight);
+
+                }
+
+
+            }
+        }
+
+
         //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
+        return endTwoHours;
     }
 
 }
