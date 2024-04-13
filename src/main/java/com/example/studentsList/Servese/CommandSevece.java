e5fe0e23fdcb1933e2bf369a730e99f2570f7e3c
Nkostya12 <135843873+Nkostya12@users.noreply.github.com>
2024-04-14 00:58:45 +0300

    eqwe

--- src/main/java/com/example/studentsList/Servese/CommandSevece.java
+++ src/main/java/com/example/studentsList/Servese/CommandSevece.java
@@ -3,6 +3,8 @@ package com.example.studentsList.Servese;
 import com.example.studentsList.listner.StudentEvent;
 import com.example.studentsList.listner.TypeEventStudent;
 import com.example.studentsList.model.Student;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
@@ -13,31 +15,46 @@ import java.util.*;
 public class CommandSevece {
 
 
-    private List<Student> listStudent = new ArrayList<>();
+
+    private final List<Student> listStudent;
 
     private final ApplicationEventPublisher publisher;
 
+    private boolean isEmpty;
 
-    public CommandSevece(ApplicationEventPublisher publisher) {
+
+    public CommandSevece(ApplicationEventPublisher publisher,  @Value("${isEmpty}") boolean isEmpty) {
+
         this.publisher = publisher;
+        listStudent = new ArrayList<>();
+        this.isEmpty = isEmpty;
+
+        if (!isEmpty) {
+            listStudent.add(new Student(1, "ivan", "ivanov", 22));
+            listStudent.add(new Student(2, "sidor", "sidorov", 29));
+            listStudent.add(new Student(3, "anton", "antonov", 40));
+        }
+
     }
 
     @ShellMethod
     public Student add(String firstName, String lastName, int age) {
         Student student = new Student(new Random().nextInt(1001), firstName, lastName, age);
+
+
         listStudent.add(student);
         publisher.publishEvent(new StudentEvent(student, TypeEventStudent.ADD));
         return student;
     }
 
     @ShellMethod
-    public List show() {
+    public List<Student> show() {
         publisher.publishEvent(new StudentEvent(listStudent, TypeEventStudent.SHOW_ALL));
         return listStudent;
     }
 
     @ShellMethod
-    public Optional remove(Integer id) {
+    public Optional<Boolean> remove(Integer id) {
         publisher.publishEvent(new StudentEvent(id, TypeEventStudent.DELETE));
         return listStudent.stream().filter(student -> student.getId() == id).
                 findAny().map(studentAny -> listStudent.remove(studentAny));
